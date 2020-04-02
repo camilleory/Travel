204222d309c26d9a3a9928e75f70015f617385a0
Camille Ory <camilla.ory@gmail.com>
2020-04-02 14:04:28 +0200

    pack section with DOM manipulation done

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//Googe Map
+
 function myMap() {
     var mapProp= {
       center:new google.maps.LatLng(51.508742,-0.120850),
@@ -5,3 +7,99 @@ function myMap() {
     };
     var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
     }
+    
+// Selection menu
+
+let select = document.querySelector('#continents')
+let health = document.querySelectorAll('.packing-stuff')[0]
+let cloth = document.querySelectorAll('.packing-stuff')[1]
+let sun = document.querySelectorAll('.packing-stuff')[2]
+let sea = document.querySelectorAll('.packing-stuff')[3]
+let documents = document.querySelectorAll('.packing-stuff')[4]
+let tourist = document.querySelectorAll('.packing-stuff')[5]
+let suitcase = document.querySelector('#suitcase');
+
+
+select.onchange = function(){
+  let selectedCountry = $( "#continents" ).val();
+  if (selectedCountry === "Asia"){
+    asia()
+  }
+  if (selectedCountry === "Africa"){
+    africa()
+  }
+  if (selectedCountry === "Europe"){
+    europe()
+  }
+  if (selectedCountry === "Americas"){
+    americas()
+  }
+  if (selectedCountry === "Australia"){
+    australia()
+  }  
+  if (selectedCountry === "choice"){
+    choice()
+  }
+};
+
+function choice (){
+  health.style.display = "none"
+  cloth.style.display = 'none';
+  sun.style.display = "none";
+  sea.style.display = "none"
+  documents.style.display = 'none';
+  tourist.style.display = 'none';  
+}
+
+function europe (){
+  health.style.display = "none"
+  cloth.style.display = 'block';
+  sun.style.display = "none";
+  sea.style.display = "none"
+  documents.style.display = 'block';
+  tourist.style.display = 'block';
+  suitcase.style.display = 'none';
+    
+}
+
+function asia (){
+  health.style.display = "none"
+  cloth.style.display = 'none';
+  sun.style.display = "block";
+  sea.style.display = "block"
+  documents.style.display = 'block';
+  tourist.style.display = 'block';  
+  suitcase.style.display = 'none';
+  
+}
+function africa (){
+  health.style.display = "block"
+  cloth.style.display = 'none';
+  sun.style.display = "block";
+  sea.style.display = "none"
+  documents.style.display = 'block';
+  tourist.style.display = 'block';    
+  suitcase.style.display = 'none';
+
+}
+function americas (){
+  health.style.display = "none"
+  cloth.style.display = 'block';
+  sun.style.display = "block";
+  sea.style.display = "none"
+  documents.style.display = 'block';
+  tourist.style.display = 'block';  
+  suitcase.style.display = 'none';
+  
+}
+
+function australia (){
+  health.style.display = "block"
+  cloth.style.display = 'none';
+  sun.style.display = "block";
+  sea.style.display = "block"
+  documents.style.display = 'block';
+  tourist.style.display = 'none'; 
+  suitcase.style.display = 'none';
+   
+}
